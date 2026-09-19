c0f8cf914d2f3b9699b8043923bd3cfa2f8068b8
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-09-19 13:12:39 -0300

    Bora lá finalizar o 1ª modulo

---
+++ 1_INTRO/Tarefa01/tarefa01.js
@@ -0,0 +1,12 @@
+console.log("Tarefa01:");
+console.log("1.Crie um novo projeto de Node.js;");
+console.log("2.Crie um arquivo para a aplicação com o nome programa;");
+console.log("3.No arquivo crie duas variáveis e imprima a soma delas;");
+console.log("4.Execute o arquivo e verifique a resposta no terminal;");
+console.log("Bora");
+console.log();
+let A = 22;
+let B = 22;
+console.log("A soma de A + B = ?", A + B)
+console.log()
+console.log("The End")
